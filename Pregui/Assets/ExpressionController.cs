1e14a98b0f1c5654896e7cd2a124c43499cb36e9
Fernando Guillen <fguillen.mail@gmail.com>
2020-05-30 23:35:44 +0200

    Pregui starts having expressions

---
+++ Pregui/Assets/ExpressionController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpressionController : MonoBehaviour
+{
+  public Animator animator;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+        
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    
+    if(Random.Range(1, 50) == 1)
+    {
+      // Change expression
+      var random = (int)Mathf.Round(Random.Range(1, 4));
+      Debug.Log("Change expression: " + random);
+      animator.SetInteger("expression", random);
+    }
+  }
+}
