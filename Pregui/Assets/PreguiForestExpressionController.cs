a75ef05a560e75b8ee76056eb24b9d479618e31b
Fernando Guillen <fguillen.mail@gmail.com>
2020-06-01 16:00:42 +0200

    Pregui in the forest also has expression

---
+++ Pregui/Assets/PreguiForestExpressionController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreguiForestExpressionController : MonoBehaviour
+{
+  public Animator animator;
+
+  void Awake() {
+    animator = GetComponent<Animator>();
+  }
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if(Random.Range(1, 50) == 1)
+    {
+      // Change expression
+      var random = (int)Mathf.Round(Random.Range(1, 4));
+      Debug.Log("Change expression: " + random);
+      animator.SetInteger("expression", random);
+    }
+  }
+}
