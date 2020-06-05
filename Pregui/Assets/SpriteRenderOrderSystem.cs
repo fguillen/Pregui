d9332704c23603c20d29fef6b23e2ff35807b080
Fernando Guillen <fguillen.mail@gmail.com>
2020-06-05 22:48:10 +0200

    Having an ordered forest (2)

--- Pregui/Assets/SpriteRenderOrderSystem.cs
+++ Pregui/Assets/SpriteRenderOrderSystem.cs
@@ -15,32 +15,58 @@ public class SpriteRenderOrderSystem : MonoBehaviour
   }
 
   static void OrderSpriteRenderers(GameObject gameObject){
-    SpriteRenderer[] elements = gameObject.GetComponentsInChildren<SpriteRenderer>();
+    SpriteRenderer[] elements = gameObject.GetComponentsInChildren<SpriteRenderer>(true);
 
     Debug.Log("elements.Count: " + elements.Count());
 
     if(elements.Count() > 0) {
       int minSortingOrder = (int)elements.Min(element => element.sortingOrder);
+      float minY = (float)elements.Min(element => BottomY(element));
       Debug.Log("minSortingOrder: " + minSortingOrder);
+      Debug.Log("minY: " + minY);
 
       foreach(SpriteRenderer element in elements){
+        BottomY(element);
         int finalSortingOrder = element.sortingOrder - minSortingOrder; // Normalize
-        finalSortingOrder += (int)(gameObject.transform.position.y * -100);
+        finalSortingOrder += (int)(minY * -1000);
         element.sortingOrder = finalSortingOrder;
       }
     }
   }
 
   static void OrderSpriteMeshInstances(GameObject gameObject){
-    SpriteMeshInstance[] elements = gameObject.GetComponentsInChildren<SpriteMeshInstance>();
+    SpriteMeshInstance[] elements = gameObject.GetComponentsInChildren<SpriteMeshInstance>(true);
     if(elements.Count() > 0) {
       int minSortingOrder = (int)elements.Min(element => element.sortingOrder);
+      float minY = (float)elements.Min(element => BottomY(element));
 
       foreach(SpriteMeshInstance element in elements){
         int finalSortingOrder = element.sortingOrder - minSortingOrder; // Normalize
-        finalSortingOrder += (int)(gameObject.transform.position.y * -100);
+        finalSortingOrder += (int)(minY * -1000);
         element.sortingOrder = finalSortingOrder;
       }
     }
+  }
+
+  static float BottomY(SpriteRenderer spriteRenderer){
+    Renderer renderer = spriteRenderer.GetComponent<Renderer>();
+
+    // Debug.Log("bounds: " + renderer.bounds.ToString());
+    // Debug.Log("centerY: " + renderer.bounds.center.y);
+    // Debug.Log("extents: " + renderer.bounds.extents.ToString());
+    // Debug.Log("extentsY: " + renderer.bounds.extents.y);
+
+    return renderer.bounds.center.y + renderer.bounds.extents.y;
+  }
+
+  static float BottomY(SpriteMeshInstance spriteRenderer){
+    Renderer renderer = spriteRenderer.GetComponent<Renderer>();
+
+    // Debug.Log("bounds: " + renderer.bounds.ToString());
+    // Debug.Log("centerY: " + renderer.bounds.center.y);
+    // Debug.Log("extents: " + renderer.bounds.extents.ToString());
+    // Debug.Log("extentsY: " + renderer.bounds.extents.y);
+
+    return renderer.bounds.center.y + renderer.bounds.extents.y;
   }
 }
