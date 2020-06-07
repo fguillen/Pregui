bbbb373818270493497f380f5b734034e624a074
Fernando Guillen <fguillen.mail@gmail.com>
2020-06-07 18:10:29 +0200

    Pregui try to open the Door

--- Pregui/Assets/Scripts/PreguiBigController.cs
+++ Pregui/Assets/Scripts/PreguiBigController.cs
@@ -10,16 +10,23 @@ public class PreguiBigController : MonoBehaviour
   private Vector2 direction;
   private Vector3 scale;
   private Animator animator;
+  public GameObject figure;
+  public GameObject figureWithFlowers;
+  public GameObject armWithFlowers;
+  public GameObject handOpen;
+  public GameObject handClosed;
+  private string state;
 
   void Awake() {
     direction = Vector2.zero;
     scale = new Vector3(1f, 1f, 1f);
     animator = gameObject.GetComponent<Animator>();
+    state = "normal";
   }
   // Start is called before the first frame update
   void Start()
   {
-
+    RenderFigure();
   }
 
   // Update is called once per frame
@@ -40,5 +47,48 @@ public class PreguiBigController : MonoBehaviour
 
     theRB.velocity = direction;
     gameObject.transform.localScale = scale;
+
+    if(Input.GetKeyDown(KeyCode.Space)) {
+      TryOpenDoor();
+    }
   }
+
+  void TryOpenDoor(){
+    state = "tryOpenDoor";
+    RenderFigure();
+    animator.SetTrigger("openDoor");
+  }
+
+  void FinishOpenDoor(){
+    state = "normal";
+    RenderFigure();
+  }
+
+  void RenderFigure(){
+    if(state == "normal") {
+      Debug.Log("normal");
+      figure.SetActive(true);
+      figureWithFlowers.SetActive(false);
+      armWithFlowers.SetActive(false);
+    }
+
+    if(state == "withFlowers") {
+      figure.SetActive(false);
+      figureWithFlowers.SetActive(true);
+      armWithFlowers.SetActive(true);
+      handClosed.SetActive(true);
+      handOpen.SetActive(false);
+    }
+
+    if(state == "tryOpenDoor") {
+      Debug.Log("tryOpenDoor");
+      figure.SetActive(false);
+      figureWithFlowers.SetActive(true);
+      armWithFlowers.SetActive(true);
+      handClosed.SetActive(false);
+      handOpen.SetActive(true);
+    }
+  }
+
+
 }
