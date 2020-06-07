02c5a3e8f73ffdba0770ddb1d20c7799f5dd9f22
Fernando Guillen <fguillen.mail@gmail.com>
2020-06-07 18:54:49 +0200

    Pregui leaves the house

---
+++ Pregui/Assets/Sprites/CanvasController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasController : MonoBehaviour
+{
+  public static CanvasController instance;
+  private Animator animator;
+
+  void Awake() {
+    instance = this;
+    animator = gameObject.GetComponent<Animator>();
+  }
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public void WhiteFadeIn(){
+    animator.SetTrigger("whiteFadeIn");
+  }
+
+  public void WhiteFadeOut(){
+    animator.SetTrigger("whiteFadeOut");
+  }
+
+  public void LoadSceneForest() {
+    SceneManager.LoadScene("Forest");
+  }
+}
---
+++ Pregui/Assets/Sprites/DoorController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+  private Animator animator;
+  private string state;
+  public GameObject doorFrame;
+  public GameObject preguiFrame;
+  private BoxCollider2D doorFrameCollider;
+  private BoxCollider2D preguiFrameCollider;
+
+  void Awake(){
+    animator = gameObject.GetComponent<Animator>();
+    doorFrameCollider = doorFrame.GetComponent<BoxCollider2D>();
+    preguiFrameCollider = preguiFrame.GetComponent<BoxCollider2D>();
+  }
+  // Start is called before the first frame update
+  void Start()
+  {
+    state = "closed";
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    CheckIfPreguiIsInTheDoor();
+  }
+
+  void OnTriggerEnter2D(Collider2D other){
+    Debug.Log("collision");
+    if(state == "closed" && other.CompareTag("PreguiHand")) {
+      Open();
+    }
+  }
+
+  void Open(){
+    Debug.Log("open");
+    animator.SetBool("open", true);
+    state = "open";
+  }
+
+  void CheckIfPreguiIsInTheDoor() {
+    if(state == "open"){
+      if(doorFrameCollider.bounds.Intersects(preguiFrameCollider.bounds)) {
+        Debug.Log("preguiOut");
+        CanvasController.instance.WhiteFadeIn();
+      }
+    }
+  }
+}
