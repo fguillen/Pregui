5dad01d53b0fd4ec961fb821eac01ab4662bcb71
Fernando Guillen <fguillen.mail@gmail.com>
2020-06-09 22:45:45 +0200

    Forest house opens and closes the door automatically

--- Pregui/Assets/Scripts/HouseForestController.cs
+++ Pregui/Assets/Scripts/HouseForestController.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class HouseForestController : MonoBehaviour
 {
+  private string state;
+  public GameObject doorHandler;
+  public GameObject pregui;
+  private Animator animator;
+
+  void Awake() {
+    state = "doorOpen";
+    animator = gameObject.GetComponent<Animator>();
+  }
+
   // Start is called before the first frame update
   void Start()
   {
@@ -13,6 +23,32 @@ public class HouseForestController : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    CheckDoorStatus();
+  }
 
+  void CheckDoorStatus() {
+    if(state == "doorClosed" && DistanceWithPregui() < 0.5f) {
+      OpenDoor();
+    }
+
+    if(state == "doorOpen" && DistanceWithPregui() > 0.5f) {
+      CloseDoor();
+    }
+  }
+
+  void CloseDoor() {
+    animator.SetBool("doorClosed", true);
+    state = "doorClosed";
+  }
+
+  void OpenDoor() {
+    animator.SetBool("doorClosed", false);
+    state = "doorOpen";
+  }
+
+  float DistanceWithPregui(){
+    float distance = Vector3.Distance(doorHandler.transform.position, pregui.transform.position);
+    Debug.Log("distance: " + distance);
+    return distance;
   }
 }
